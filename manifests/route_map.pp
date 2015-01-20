2d702c33d70bb7a4f03acac512a1e3e2fc520c27
Nate Riffe <inkblot@movealong.org>
2015-01-19 19:02:05 -0600

    Add a quantum of route map support

---
+++ manifests/route_map.pp
@@ -0,0 +1,11 @@
+# ex: syntax=puppet si sw=4 ts=4 et
+define quagga::route_map (
+    $entries = {},
+    $service,
+) {
+    concat::fragment { "quagga-route-map-${name}":
+        target  => "/etc/quagga/${service}.conf",
+        order   => "30",
+        content => template('quagga/route_map.erb'),
+    }
+}
