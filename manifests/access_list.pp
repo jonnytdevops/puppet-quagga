22755aa0feac4034c922478f3c21d1dd471b4d1e
Michael Hashizume <michael.hashizume@puppet.com>
2020-09-14 16:03:24 -0700

    Minor linting updates

--- manifests/access_list.pp
+++ manifests/access_list.pp
@@ -1,12 +1,12 @@
 # ex: syntax=puppet si sw=4 ts=4 et
 define quagga::access_list (
+    $service,
     $networks = [],
     $action   = 'permit',
-    $service,
 ) {
     concat::fragment { "quagga-access-list-${name}":
         target  => "/etc/quagga/${service}.conf",
-        order   => "20",
+        order   => '20',
         content => template('quagga/access_list.erb'),
     }
 }
--- manifests/route_map.pp
+++ manifests/route_map.pp
@@ -1,11 +1,11 @@
 # ex: syntax=puppet si sw=4 ts=4 et
 define quagga::route_map (
-    $entries = {},
     $service,
+    $entries = {},
 ) {
     concat::fragment { "quagga-route-map-${name}":
         target  => "/etc/quagga/${service}.conf",
-        order   => "30",
+        order   => '30',
         content => template('quagga/route_map.erb'),
     }
 }
